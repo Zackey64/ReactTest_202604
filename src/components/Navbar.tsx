b318cf9bb9a75c949d5a504e1f9664bfd820f64b
Zackey64 <ishizaki0826@gmail.com>
2026-04-09 21:16:39 +0900

    変更２

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="mx-auto flex max-w-5xl
-     p-4 bg-sub/80 text-sub-text backdrop-blur-md
-     sticky top-0 z-50 w-">
+    <nav className="mx-auto flex w-full
+     p-2 bg-sub/70 text-sub-text backdrop-blur-md
+     sticky top-0 z-50">
         
         <div className="space-x-6">
             <Link href="/" className="hover:text-accent-text transition-colors">
